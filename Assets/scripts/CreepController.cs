b0c07b335783403f2684d32261f4f2b7ae77bc78
Zichuan <zainyu717@outlook.com>
2018-09-11 03:45:18 -0400

    alpha

---
+++ Assets/scripts/CreepController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreepController : MonoBehaviour {
+
+  public PlayerControl player;
+  public CreepMoveConfig creepMoveConfig;
+  public AttackConfig attackConfig;
+
+  [System.Serializable]
+  public class CreepMoveConfig
+  {
+    public float totalSpeed = 1.0f;
+    public float catchPlayerFactor;
+    public float keepDistanceFactor;
+    public float keepDistanceRange;
+    public float goCenterFactor;
+    public float goCenterRange;
+  }
+
+  [System.Serializable]
+  public class AttackConfig
+  {
+    public float searchEnemyRange = 1.0f;
+  }
+
+  private new Rigidbody rigidbody;
+
+  private CreepController nearestEnemy;
+
+  // Use this for initialization
+  void Start () {
+    rigidbody = GetComponent<Rigidbody>();
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+
+  void FixedUpdate() {
+    if (player == null) {
+      return;
+    }
+    // List<CreepController> neighbors = new List<CreepController>();
+    Vector3 keepDistance = new Vector3();
+    Vector3 creepCenter = new Vector3();
+    int creepCenterCount = 0;
+
+    foreach (CreepController creep in player.GetCreeps()) {
+      if (creep == this) continue;
+      float distance = Vector3.Distance(creep.transform.position, transform.position);
+      if (distance < creepMoveConfig.keepDistanceRange) {
+        keepDistance += (transform.position - creep.transform.position).normalized * (creepMoveConfig.keepDistanceRange - distance);
+      }
+
+      if (distance < creepMoveConfig.goCenterRange)
+      {
+        creepCenter += creep.transform.position;
+        ++creepCenterCount;
+      }
+    }
+    keepDistance *= creepMoveConfig.keepDistanceFactor;
+    creepCenter /= creepCenterCount;
+
+    // TODO 改成：判断一下速度点成，但是都至少有0.5
+    Vector3 goCenter = (creepCenter - transform.position) * creepMoveConfig.goCenterFactor;
+    /*
+    Vector3 playerSpeed = player.rigidbody.velocity;
+    float dotProductFactor = Vector3.Dot((transform.position - player.transform.position).normalized, playerSpeed.normalized);
+    dotProductFactor = (dotProductFactor + 1) / 2;
+    */
+    Vector3 catchPlayer = (player.transform.position - transform.position) * creepMoveConfig.catchPlayerFactor * Vector3.Distance(player.transform.position, transform.position);
+    Vector3 vel = (keepDistance + goCenter + catchPlayer) * creepMoveConfig.totalSpeed;
+    vel.y = 0.0f;
+    rigidbody.velocity = vel;
+
+
+    // handle enemy
+    nearestEnemy = null;
+    float curNearestDistance = 999999.0f;
+    foreach (PlayerControl other_player in player.gameController.players) {
+      if (other_player == player) continue;
+      foreach (CreepController creep in other_player.GetCreeps())
+      {
+        float distance = Vector3.Distance(creep.transform.position, transform.position);
+        if (distance < attackConfig.searchEnemyRange && distance < curNearestDistance)
+        {
+          curNearestDistance = distance;
+          nearestEnemy = creep;
+        }
+      }
+    }
+    if (nearestEnemy != null) {
+      transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+    }
+  }
+
+  public void SetPlayer(PlayerControl player_in)
+  {
+    player = player_in;
+  }
+}
+
+
+
+
---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+  public SpeedConfig speedConfig;
+  public int playerNumber;
+  public int initialCreepNumber = 5;
+  public GameObject creepPrefab;
+  public CreepGenerationConfig creepGenerationConfig;
+  public GameController gameController;
+  public bool disableControl = false;
+
+  [System.Serializable]
+  public class SpeedConfig
+  {
+    public float moveSpeed;
+    public float dashSpeed;
+  }
+
+  [System.Serializable]
+  public class CreepGenerationConfig
+  {
+    public float creepGenerationRange;
+    public float yOffset;
+  }
+
+  [SerializeField]
+  private int creepCount = 0;
+
+  private HashSet<CreepController> creeps;
+  [HideInInspector]
+  public new Rigidbody rigidbody;
+
+  // Use this for initialization
+  void Start () {
+    rigidbody = GetComponent<Rigidbody>();
+    creeps = new HashSet<CreepController>();
+    for (int i = 0; i < initialCreepNumber; ++i) {
+      GenNewCreep();
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetKeyDown(KeyCode.Return)) {
+      GenNewCreep();
+    }
+	}
+
+  void FixedUpdate() {
+    float moveH = Input.GetAxis("Horizontal");
+    float moveV = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+    if (disableControl) {
+      movement = new Vector3();
+    }
+    rigidbody.velocity = movement.normalized * speedConfig.moveSpeed;
+  }
+
+  public HashSet<CreepController> GetCreeps() {
+    return creeps;
+  }
+
+  public void AddCreep(CreepController creep) {
+    if (!creeps.Contains(creep)) {
+      ++creepCount;
+      creeps.Add(creep);
+    }
+  }
+
+  // reborn a new creep
+  public void GenNewCreep() {
+    Vector3 creepPosOffset = new Vector3(Random.Range(-1.0f, 1.0f), creepGenerationConfig.yOffset, Random.Range(-1.0f, 1.0f)) * creepGenerationConfig.creepGenerationRange;
+    CreepController creep = Instantiate(creepPrefab, transform.position + creepPosOffset, Quaternion.identity).GetComponent<CreepController>();
+    creep.SetPlayer(this);
+    AddCreep(creep);
+  }
+}
+
